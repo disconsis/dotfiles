d7a82f1ec6b077a95fb70f05d66254408dbcf980
DISS0NANCE <ketan.kanishka.cse15@iitbhu.ac.in>
2017-09-10 14:03:39 +0530

    added regex to better identify apps

--- .config/i3/test/find_apps.py
+++ .config/i3/test/find_apps.py
@@ -1,11 +1,13 @@
+#!/usr/bin/python3
+
 # TODO: implement locking mechanism
 # TODO: fix last app remaining on closing all of them
 
-from glyphs import glyph_mapping
 import subprocess as proc
 import i3ipc
 from time import sleep
 from collections import defaultdict
+import re
 
 
 def classify_windows(i3):
@@ -17,22 +19,65 @@ def classify_windows(i3):
 
 
 def find_apps(windows):
-    if not len(windows):
-        return ['-']
     apps = []
     for window in windows:
-        found = False
-        for entry in glyph_mapping:
-            for app in entry:
-                if app in window.name.lower():
-                    apps.append(glyph_mapping[entry])
-                    found = True
-                    break
-            if found:
-                break
-    if not len(apps):
-        apps = ['?']
+        if window.name is None:
+            continue
+        app = get_app(window.name)
+        if app is not None:
+            apps.append(app)
     return apps
+
+
+def get_app(title):
+    download_regex = re.compile('^uGet( - (\d+) tasks)?$')
+    match = download_regex.fullmatch(title)
+    if match is None:
+        pass
+    elif match.groups() == (None, None):
+        return ''
+    else:
+        num_tasks = match.group(2)
+        print(match.groups())
+        return ' +{}'.format(num_tasks)
+
+    browser_regex = [
+        re.compile('^((.+) - )?Mozilla Firefox$'),
+        re.compile('^((.+) - )?Vimperator$'),
+    ]
+    for reg in browser_regex:
+        match = reg.fullmatch(title)
+        if match is None:
+            continue
+        elif len(match.groups()) == 1:  # implies no subtitle
+            return ''
+        else:
+            yt_regex = re.compile('^(.+ - )?YouTube$')
+            if yt_regex.fullmatch(str(match.group(2))):
+                return ''
+            return ''
+
+    okular_regex = re.compile('^(.+ – )?Okular$')  # IMP: the dash is abnormal
+    if okular_regex.fullmatch(title):
+        return ''
+
+    vm_regex = [
+        re.compile('^(.+ - )?VMware Workstation 12 Player (Non-commercial use only)$'),
+        re.compile('^(.+ (\[.+\]) - )?Oracle VM VirtualBox Manager$')
+    ]
+    for reg in vm_regex:
+        if reg.fullmatch(title):
+            return ''
+
+    media_regex = re.compile('^(.* - )?VLC media player$')
+    if media_regex.fullmatch(title):
+        return ''
+
+    # TODO: steam
+    if title == 'Terminal':
+        return ''
+
+    return None
 
 
 def get_new_name(workspace, apps):
@@ -51,6 +96,8 @@ def get_new_name(workspace, apps):
 
 
 def rename_workspace(i3, workspace, windows):
+    if not len(windows):
+        return
     apps = find_apps(windows)
     try:
         new_name = get_new_name(workspace, apps)
@@ -58,10 +105,10 @@ def rename_workspace(i3, workspace, windows):
         proc.call(['i3-nagbar', '-m',
                    '"too many `:` in workspace {}"'.format(workspace.num)])
         return
-
-    if new_name != workspace.name:
-        i3.command('rename workspace "{}" to "{}"'.format(workspace.name,
-                                                          new_name))
+    else:
+        if new_name != workspace.name:
+            i3.command('rename workspace "{}" to "{}"'.format(workspace.name,
+                                                              new_name))
 
 
 if __name__ == '__main__':
