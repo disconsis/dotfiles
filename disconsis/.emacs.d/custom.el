1164be859551d906a65b082e3070721e1b32795a
disconsis <ketan.kanishka.cse15@iitbhu.ac.in>
2018-12-26 23:52:48 +0530

    add emacs config

---
+++ disconsis/.emacs.d/custom.el
@@ -0,0 +1,30 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("f66ffeadda7b52d40c8d698967ae9e9836f54324445af95610d257fa5e3e1e21" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "b73a23e836b3122637563ad37ae8c7533121c2ac2c8f7c87b381dd7322714cd0" "4138944fbed88c047c9973f68908b36b4153646a045648a22083bd622d1e636d" "a4b062aa2d99859d1449440d7f40547efdaf3fec73e9862b4c3f45a18cff44c8" "ef98b560dcbd6af86fbe7fd15d56454f3e6046a3a0abd25314cfaaefd3744a9e" "7d2e7a9a7944fbde74be3e133fc607f59fdbbab798d13bd7a05e38d35ce0db8d" "36c86cb6c648b9a15d849026c90bd6a4ae76e4d482f7bcd138dedd4707ff26a5" "f27c3fcfb19bf38892bc6e72d0046af7a1ded81f54435f9d4d09b3bff9c52fc1" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "98f44183ce2b3b730a99e3853e4223aaa7343bb18e4108b1af11d04bdaf4059f" default)))
+ '(menu-bar-mode nil)
+ '(menu-bar-no-scroll-bar nil)
+ '(package-selected-packages
+   (quote
+    (flycheck-ghcmod projectile git-gutter+ origami pretty-mode eyebrowse hideshow-org git-gutter-fringe diff-hl flymake-haskell-multi haskell-mode smart-mode-line-atom-one-dark-theme smart-mode-line one-themes ujelly-theme nimbus-theme autopair adjust-parens which-key org-bullets challenger-deep-theme pdf-tools gruvbox-theme rainbow-delimiters evil-commentary evil-collection evil-surround color-theme-sanityinc-tomorrow magit use-package org-link-minor-mode helm evil-visual-mark-mode)))
+ '(scroll-bar-mode nil)
+ '(tool-bar-mode nil))
+
+;; fonts
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:slant normal :weight light :height 80 :width normal :family "Monoid"))))
+ '(bold ((t (:weight bold))))
+ '(italic ((t (:inherit default :weight light :slant italic :height 105 :foundry "H&Co" :family "Operator Mono"))))
+ '(bold-italic ((t (:inherit italic :weight normal))))
+ '(border ((t (:background nil))))
+ '(fringe ((t (:background nil))))
+ '(rainbow-delimiters-mismatched-face ((t (:background "red"))))
+ '(rainbow-delimiters-unmatched-face ((t (:background "red")))))
